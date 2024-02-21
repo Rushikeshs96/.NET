9054cf49c4301c726b760361084c724f65a1ed49
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-02-21 12:25:03 +0530

    category controller and view added

---
+++ BookManagmentMVC/BookManagment/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BookManagment.Data;
+using BookManagment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookManagment.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> objCategorylist = _db.categories.ToList();
+            return View();
+        }
+    }
+}
