9f8597c4e28c7e43025d07e44afc19f3d9671fe3
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-03-05 19:31:09 +0530

    book controller created

--- DOTNET-JWT/Controllers/BookController.cs
+++ DOTNET-JWT/Controllers/BookController.cs
@@ -25,5 +25,91 @@ namespace DOTNET_JWT.Controllers
             }
             return await _jwtContext.Books.ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> GetBook(int id)
+        {
+            if (_jwtContext.Books == null)
+            {
+                return NotFound();
+            }
+            var movie = await _jwtContext.Books.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return movie;
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBook(int id, Book book)
+        {
+            if (id != book.Id)
+            {
+                return BadRequest();
+            }
+
+            _jwtContext.Entry(book).State = EntityState.Modified;
+
+            try
+            {
+                await _jwtContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BookExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+       
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostMovie(Book book)
+        {
+            if (_jwtContext.Books== null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Book'  is null.");
+            }
+            _jwtContext.Books.Add(book);
+            await _jwtContext.SaveChangesAsync();
+
+            return CreatedAtAction("GetBook", new { id = book.Id }, book);
+        }
+
+       
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBook(int id)
+        {
+            if (_jwtContext.Books == null)
+            {
+                return NotFound();
+            }
+            var book = await _jwtContext.Books.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            _jwtContext.Books.Remove(book);
+            await _jwtContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BookExists(int id)
+        {
+            return (_jwtContext.Books?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
