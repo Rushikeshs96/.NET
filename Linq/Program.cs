b2b60bbc35f8389f1bc9ce2e0b78539330fa8c89
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-07 13:07:49 +0530

    sorting linq opeartaions

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -2,7 +2,7 @@
 {
   internal class Program
     {
-        public static void Main(String[] args)
+        public static void Main1(String[] args)
         {
             //Datasource
             int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
---
+++ Linq/Sorting.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Sorting
+    {
+        public static void Main(string[] args)
+        {
+            string[] words = { "the", "quick", "brown", "fox", "jumps" };
+//1
+            // sorting by length of word - query syntaxt
+            var length = from word in words
+                         orderby word.Length descending
+                         select word;
+
+            // lambad expression
+            var len = words.OrderBy(x => x.Length);
+
+            foreach(var word in len)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+
+//2
+            // sorting by length of word descending - query syntaxt
+            var wordlen = from word in words
+                         orderby word.Length descending
+                         select word;
+
+            // lambad expression
+            var wordl = words.OrderByDescending(x => x.Length);
+
+            foreach (var word in wordl)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+
+//3
+            // sorting on alphabetical order of first character
+            var alphabet = from word in words
+                          orderby word.Substring(0,1)
+                          select word;
+
+            // lambad expression
+            var alpha = words.OrderBy(x=>x.Substring(0,1));
+
+            foreach (var word in alpha)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+
+//4
+            // sorting on alphabetical order of first character and length
+            var alphalen = from word in words
+                           orderby word.Length, word.Substring(0,1)
+                           select word;
+
+            // lambad expression
+            //var lenalpha = words.OrderBy(x =>x.Length || x.Substring(0,1);
+
+            foreach (var word in alphalen)
+            {
+                Console.WriteLine(word);
+            }
+
+            Console.WriteLine();
+
+
+        }
+    }
+}
