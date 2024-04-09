34e5cbf2e9d3f492b33df98a2e506dfb53ee259a
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-09 12:17:14 +0530

    update

---
+++ Linq/CountofwordusingLinq.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class CountofwordusingLinq
+    {
+        public static void Main(string[] args)
+        {
+            String text = " this is sample text, to check occurence of word sample in sample text ! Thank you";
+
+            String searchword = "sample";
+
+            String[] textArray = text.Split(new char[] { ' ', ',', '.' });
+
+            var query = from word in textArray
+                        where word.Equals(searchword)
+                        select word;
+
+            Console.WriteLine("count of word sample = " + query.Count());
+
+
+            //extracting only numbers from string
+
+            String str = "abcd12-3-d-468$";
+
+            var result=from ch in str
+                       where char.IsDigit(ch)
+                       select ch;
+
+            foreach( var ch in result ) {
+                Console.WriteLine(ch);
+                    }
+        }
+    }
+}
