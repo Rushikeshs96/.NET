765cbc9ba5b05bf71ffa5e3ea6c56e7d2d5f1be4
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-11 22:07:35 +0530

    update

---
+++ Dot Net WebApi/Models/Login.cs
@@ -0,0 +1,10 @@
+﻿namespace Dot_Net_WebApi.Models
+{
+    public class Login
+    {
+        public int Id { get; set; }
+        public string Mail { get; set; }
+        public string Password { get; set; }
+       
+    }
+}
