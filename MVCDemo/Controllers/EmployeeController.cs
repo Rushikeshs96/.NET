7c3a7bd5d101e1af5c8bc4cab0abe0e12878d488
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-14 15:20:18 +0530

    model class creation

---
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.Models;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Employee emp = new Employee()
+            {
+                Id = 1,
+                Name = "rushi",
+                Gender = "male",
+                City = "asu"
+            };
+
+            return View(emp);
+        }
+    }
+}
---
+++ MVCDemo/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿namespace MVCDemo.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public string Gender { get; set; }
+
+        public string City { get; set; }
+    }
+}
