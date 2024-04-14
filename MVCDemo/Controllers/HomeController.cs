eaa8354bbe24408e5de1c6e376862ad191d920e4
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-14 14:36:49 +0530

    MVCdemo

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MVCDemo.Models;
+using System.Diagnostics;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            ViewBag.Country = new List<String>()
+            {
+                "India",
+                "China",
+                "Japan",
+                "Korea"
+            };
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    
+    }
+}
