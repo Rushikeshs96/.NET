c843ec459da9bb2b5e6c8758cbc7501e1bde7f75
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-14 15:04:47 +0530

    viewdata learnt

--- MVCDemo/Controllers/HomeController.cs
+++ MVCDemo/Controllers/HomeController.cs
@@ -11,12 +11,20 @@ namespace MVCDemo.Controllers
 
         public IActionResult Index()
         {
-            ViewBag.Country = new List<String>()
+            ViewBag.Country = new List<String>()     //viewbag uses dynamic properties. internally viewbag properties are stores as name/value pair in viewdata disctionery.
             {
                 "India",
                 "China",
                 "Japan",
                 "Korea"
+            };
+
+            ViewData["City"] = new List<String>()     //uses string keys. viewdata is dictionery of objects
+            {
+                "Pune",
+                "Mumbau",
+                "Delhie",
+                "satara"
             };
 
             return View();
