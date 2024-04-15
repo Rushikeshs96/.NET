e0cdd3217c7b86436692e7f4b6c60d9271cb51ec
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-15 13:19:49 +0530

    connection succesful

--- MVCDemo/Program.cs
+++ MVCDemo/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MVCDemo.DAL;
+
 namespace MVCDemo
 {
     public class Program
@@ -5,6 +8,10 @@ namespace MVCDemo
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddDbContext<EmployeeContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeConnection")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
