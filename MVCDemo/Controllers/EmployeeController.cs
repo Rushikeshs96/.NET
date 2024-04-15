01e76139171f874aaa43b3414e155385070d814a
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-04-15 14:32:57 +0530

    details method

--- MVCDemo/Controllers/EmployeeController.cs
+++ MVCDemo/Controllers/EmployeeController.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVCDemo.DAL;
 using MVCDemo.Models;
 
 namespace MVCDemo.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly EmployeeContext _context;
+
+        public EmployeeController(EmployeeContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             Employee emp = new Employee()
@@ -15,6 +23,12 @@ namespace MVCDemo.Controllers
                 City = "asu"
             };
 
+            return View(emp);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var emp= _context.Employees.FirstOrDefault(s=>s.Id==id);
             return View(emp);
         }
     }
