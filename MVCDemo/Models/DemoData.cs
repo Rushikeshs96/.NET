fa2373bab24d7aed1b9514f0948f441eb4b317e6
Rushikesh Sapkal <sapkalrushikesh96@gmail.com>
2024-05-06 23:28:40 +0530

    confirming mails using another confirm field without saving it to database

--- MVCDemo/Models/DemoData.cs
+++ MVCDemo/Models/DemoData.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Net.Mail;
 
 namespace MVCDemo.Models
 {
-    public partial class DemoData
+    public class DemoData
     {
         [Key]
         [HiddenInput(DisplayValue =false)]
@@ -14,6 +15,13 @@ namespace MVCDemo.Models
         [DataType(DataType.EmailAddress)]
         [Display(Name = "Mail Adress")]
         public string? EmailAdress { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Confirm Mail")]
+        [Compare("EmailAdress", ErrorMessage = "The email and confirmation email do not match.")]
+        [DisplayName("Confirm Mail")]
+        [NotMapped] // This annotation is important to tell EF Core not to include this field in the database
+        public string? ConfirmMail { get; set; }
 
         [StringLength(10,MinimumLength =10)]
         public string? PhoneNumber { get; set; }
